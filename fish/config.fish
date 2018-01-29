31780e9e34d02a3840d5d07a092c343c70ac84dd
k-masatany <masatani@fusic.co.jp>
2018-01-29 16:21:55 +0900

    add fish shell configure file.

---
+++ fish/config.fish
@@ -0,0 +1,90 @@
+# peco & ghq
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)'
+  bind \cg  peco_select_ghq_repository
+end
+
+# anyenv
+set -x PATH $HOME/.anyenv/bin $PATH
+
+# rbenv
+set -x RBENV_ROOT "$HOME/.anyenv/envs/rbenv"
+set -x PATH $PATH "$RBENV_ROOT/bin"
+set -gx PATH '/Users/k-masatany/.anyenv/envs/rbenv/shims' $PATH
+set -gx RBENV_SHELL fish
+source '/Users/k-masatany/.anyenv/envs/rbenv/libexec/../completions/rbenv.fish'
+command rbenv rehash 2>/dev/null
+function rbenv
+  set command $argv[1]
+  set -e argv[1]
+
+  switch "$command"
+  case rehash shell
+    source (rbenv "sh-$command" $argv|psub)
+  case '*'
+    command rbenv "$command" $argv
+  end
+end
+
+# goenv
+set -x GOROOT /Users/k-masatany/.anyenv/envs/goenv/versions/1.9.2
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
+set -x GOENV_ROOT "$HOME/.anyenv/envs/goenv"
+set -x PATH $PATH "$GOENV_ROOT/bin"
+set -gx PATH '/Users/k-masatany/.goenv/shims' $PATH
+set -gx GOENV_SHELL fish
+source '/Users/k-masatany/.anyenv/envs/goenv/libexec/../completions/goenv.fish'
+command goenv rehash 2>/dev/null
+function goenv
+  set command $argv[1]
+  set -e argv[1]
+
+  switch "$command"
+  case rehash shell
+    source (goenv "sh-$command" $argv|psub)
+  case '*'
+    command goenv "$command" $argv
+  end
+end
+
+# nodenv
+set -x NODENV_ROOT "$HOME/.anyenv/envs/nodenv"
+set -x PATH $PATH "$NODENV_ROOT/bin"
+set -gx PATH '/Users/k-masatany/.nodenv/shims' $PATH
+set -gx NODENV_SHELL fish
+source '/Users/k-masatany/.anyenv/envs/nodenv/libexec/../completions/nodenv.fish'
+command nodenv rehash 2>/dev/null
+function nodenv
+  set command $argv[1]
+  set -e argv[1]
+
+  switch "$command"
+  case rehash shell
+    source (nodenv "sh-$command" $argv|psub)
+  case '*'
+    command nodenv "$command" $argv
+  end
+end
+
+# rust
+set -x PATH $PATH "$HOME/.cargo/bin"
+set -x PATH $PATH "/usr/local/sbin"
+set -x PATH $PATH "/usr/local/opt/make/libexec/gnubin"
+
+# java
+set --export JAVA_HOME `/System/Library/Frameworks/JavaVM.framework/Versions/A/Commands/java_home -v "1.8"`
+set --export PATH "$JAVA_HOME/bin" $PATH
+
+# alias
+alias be "bundle exec"
+alias rue "rbenv exec"
+alias goe "goenv exec"
+alias nde "nodenv exec"
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /Users/k-masatany/.ghq/github.com/k-masatany/master-up/node_modules/tabtab/.completions/serverless.fish ]; and . /Users/k-masatany/.ghq/github.com/k-masatany/master-up/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /Users/k-masatany/.ghq/github.com/k-masatany/master-up/node_modules/tabtab/.completions/sls.fish ]; and . /Users/k-masatany/.ghq/github.com/k-masatany/master-up/node_modules/tabtab/.completions/sls.fish
